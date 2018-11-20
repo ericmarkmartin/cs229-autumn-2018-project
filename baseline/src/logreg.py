dd4d1df3f22bd8234378e0c97dc7f32a050c8b8c
Jonathan Zwiebel <jonathanzwiebel@gmail.com>
2018-11-19 23:02:21 -0800

    Added bagofwords and stopwords

--- baseline/src/logreg.py
+++ baseline/src/logreg.py
@@ -2,22 +2,33 @@
 
 import numpy as np
 from sklearn import linear_model
+from sklearn.model_selection import train_test_split
 import preprocessing as p
 
-X, y = p.get_binary()
-logreg = linear_model.LogisticRegression()
-logreg.fit(X[::2], np.ravel(y[::2]))
+X, y = p.get_bag_of_words(stop_words=True)
 
-predictions = logreg.predict(X[1::2])
-truth = y[1::2]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=229)
+logreg = linear_model.LogisticRegression()
+logreg.fit(X_train, np.ravel(y_train))
+
+predictions = logreg.predict(X_test)
 predictions = predictions.reshape((-1, 1))
 
 print(predictions.shape)
-print(truth.shape)
+print(y_test.shape)
 
-total = len(truth)
-correct = 0
-for i in range(len(truth)):
-	if predictions[i] == truth[i]:
-		correct += 1
-print(str(correct) + " correct out of " + str(total) + ": " + str(correct/total))
+confusion_matrix = np.zeros((2,2))
+for i in range(len(y_test)):
+	confusion_matrix[y_test[i][0]][predictions[i][0]] += 1
+
+
+print("Confusion Matrix:\n" + str(confusion_matrix))
+print("Accuracy: " + str((confusion_matrix[0][0] + confusion_matrix[1][1]) / np.sum(confusion_matrix)))
+
+
+precision = confusion_matrix[1][1] / (confusion_matrix[0][1] + confusion_matrix[1][1])
+print("Precision: " + str(precision))
+recall = confusion_matrix[1][1] / (confusion_matrix[1][0] + confusion_matrix[1][1])
+print("Recall: " + str(recall))
+f1 = 2 * precision * recall / (precision + recall)
+print("F1: " + str(f1))
--- baseline/src/preprocessing.py
+++ baseline/src/preprocessing.py
@@ -5,16 +5,21 @@ DICTIONARY = "data/raw/20k.txt"
 MOVIES_POS = "data/raw/rt-polaritydata/rt-polarity.pos"
 MOVIES_NEG = "data/raw/rt-polaritydata/rt-polarity.neg"
 
+STOP_WORDS = ["a", "an", "and", "are", "as", "at", "be", "by", "for", "from", "has", "he", "in", "is", "it", "its", "of", "on", "that", "the", "to", "was", "were", "will", "with"]
+
 # Generates a word_to_index and index_to_word dictionary based
 # on a text file of words. Does not generate UNK token or filter
 # still words. 
-def read_dict():
+def read_dict(stop_words = False):
 	word_to_index = {}
 	index_to_word = {}
 
 	index = 0
 	with open(DICTIONARY, "r") as d:
 		for line in d:
+			if stop_words and line[0:-1] in STOP_WORDS:
+				continue
+
 			word_to_index[line[0:-1]] = index
 			index_to_word[index] = line[0:-1]
 			index += 1
@@ -37,6 +42,7 @@ def load_movies_as_raw_list():
 def raw_list_to_binary(raw_list, word_to_index):
 	X = np.empty((len(raw_list), len(word_to_index)), dtype=int)
 	y = np.empty((len(raw_list), 1), dtype=int)
+
 	for i, line in enumerate(raw_list):
 		X[i] = np.zeros((len(word_to_index)))
 		for word in line[0].split():
@@ -45,19 +51,29 @@ def raw_list_to_binary(raw_list, word_to_index):
 		y[i] = line[1]
 	return X, y
 
-def get_binary():
-	word_to_index, index_to_word = read_dict()
+# Converts a raw list of tuples from from setences to labels to a 
+# bag-of-words frequency vectorization of the input sentence
+def raw_list_to_bag_of_words(raw_list, word_to_index):
+	X = np.empty((len(raw_list), len(word_to_index)), dtype=int)
+	y = np.empty((len(raw_list), 1), dtype=int)
+
+	for i, line in enumerate(raw_list):
+		X[i] = np.zeros((len(word_to_index)))
+		for word in line[0].split():
+			if word in word_to_index:
+				X[i][word_to_index[word]] += 1
+		y[i] = line[1]
+	return X, y
+
+def get_binary(stop_words=False):
+	word_to_index, index_to_word = read_dict(stop_words)
 	raw_list = load_movies_as_raw_list()
 	X, y = raw_list_to_binary(raw_list, word_to_index)
 	return X, y
 
-# Don't use
-# def data_split(data, train_p, dev_p, test_p):
-# 	size = len(data)
-
-# 	random.seed(229)
-# 	random.shuffle(data)
-# 	train_size = int(train_p * size)
-# 	dev_size = int(dev_p * size)
-# 	test_size = size - train_size - dev_size
-# 	return data[:train_size], data[train_size:train_size + dev_size], data[train_size + dev_size:]
+def get_bag_of_words(stop_words=False):
+	word_to_index, index_to_word = read_dict(stop_words)
+	raw_list = load_movies_as_raw_list()
+	X, y = raw_list_to_bag_of_words(raw_list, word_to_index)
+	print(X[0])
+	return X, y
