4fa776b7f59a5165cbcd034a798160a2dce0c9ca
Eric Mark Martin <eric.martin@cheddar.com>
2018-12-10 12:42:54 -0800

    Update Makefile to make merged dataset

--- data/tokenizer.py
+++ data/tokenizer.py
@@ -1,68 +1,77 @@
 import click
 
-DICTIONARY = "data/raw/20k.txt"
+DICTIONARY = "data/external/20k.txt"
 NUMBERS = set("0123456789")
 
-def read_dict():
-	word_to_index = {}
-	index_to_word = {}
+def read_dict(dictionary):
+    word_to_index = {}
+    index_to_word = {}
 
-	index = 0
-	with open(DICTIONARY, "r") as d:
-		for line in d:
-			word_to_index[line[0:-1]] = index
-			index_to_word[index] = line[0:-1]
-			index += 1
-	return word_to_index, index_to_word
+    index = 0
+    with open(dictionary, "r") as d:
+        for line in d:
+            word_to_index[line[0:-1]] = index
+            index_to_word[index] = line[0:-1]
+            index += 1
+    return word_to_index, index_to_word
 
-def tokenize(word):
-	if word.lower() in word_to_index:
-		return word_to_index[word.lower()]
-	if any((c in NUMBERS) for c in word):
-		return NUMBER
-	if word == ",":
-		return COMMA
-	if word == ";":
-		return SEMICOLON
-	if word[0].isupper():
-		return PROPER
-	return UNKNOWN
+def tokenize(word, word_to_index, NUMBER, COMMA, SEMICOLON, UNKNOWN, PROPER):
+    if word.lower() in word_to_index:
+        return word_to_index[word.lower()]
+    if any((c in NUMBERS) for c in word):
+        return NUMBER
+    if word == ",":
+        return COMMA
+    if word == ";":
+        return SEMICOLON
+    if word[0].isupper():
+        return PROPER
+    return UNKNOWN
 
-def tokenize_file(filename):
-	file = open(filename, "r")
-	lines = file.readlines()
-	tokenized_sentences = []
+def tokenize_file(filename, dictionary):
+    word_to_index, index_to_word = read_dict(dictionary)
+    standard_count = len(word_to_index)
+    NUMBER = standard_count
+    COMMA = standard_count + 1
+    SEMICOLON = standard_count + 2
+    UNKNOWN = standard_count + 3
+    PROPER = standard_count + 4
 
-	for line in lines:
-		sentence, punctuation = eval(line)
-		new_sentence = []
-		first = True
-		for word in sentence:
-			token = tokenize(word)
-			if first and token == PROPER:
-				token = UNKNOWN
-			new_sentence.append(token)
-			first = False
-		tokenized_sentences.append((new_sentence, punctuation))
+    file = open(filename, "r")
+    lines = file.readlines()
+    tokenized_sentences = []
 
-	file.close()
-	return tokenized_sentences
+    for line in lines:
+        sentence, punctuation = eval(line)
+        new_sentence = []
+        first = True
+        for word in sentence:
+            token = tokenize(
+                            word,
+                            word_to_index,
+                            NUMBER,
+                            COMMA,
+                            SEMICOLON,
+                            UNKNOWN,
+                            PROPER
+                        )
 
+            if first and token == PROPER:
+                token = UNKNOWN
+            new_sentence.append(token)
+            first = False
+        tokenized_sentences.append((new_sentence, punctuation))
 
-word_to_index, index_to_word = read_dict()
-standard_count = len(word_to_index)
-NUMBER = standard_count
-COMMA = standard_count + 1
-SEMICOLON = standard_count + 2
-UNKNOWN = standard_count + 3
-PROPER = standard_count + 4
+    file.close()
+    return tokenized_sentences
 
 
 @click.command()
 @click.argument("filename", type=click.Path())
+@click.argument("dictionary", type=click.Path())
 @click.argument("output", type=click.Path())
-def process_file(filename, output):
-    tokenized_sentences = tokenize_file(filename)
+def process_file(filename, dictionary, output):
+    tokenized_sentences = tokenize_file(filename, dictionary)
     with open(output, 'w') as out_file:
         for sentence in tokenized_sentences:
             out_file.write(str(sentence) + '\n')
