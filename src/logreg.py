93bb2b40dc34de7a7dcd054e22b59efe92a95b6a
Eric Mark Martin <eric.martin@cheddar.com>
2018-12-11 01:12:01 -0800

    Updates

--- src/logreg.py
+++ src/logreg.py
@@ -1,10 +1,11 @@
 import vectorize
 import numpy as np
 import scipy as sp
-from sklearn import linear_model, metrics
+from sklearn import linear_model
+from sklearn.metrics import classification_report
 
 
-X, y = vectorize.tokens_to_bag_of_words("../data/processed/merged_tok.txt", one_hot_y=False)
+X, y = vectorize.tokens_to_binary("../data/processed/merged_tok.txt", one_hot_y=False)
 (X_train, y_train), (X_dev, y_dev), (X_test, y_test) = vectorize.train_dev_test_split(X, y, dev_size=0.05, test_size=0.05)
 
 print("Making logreg")
@@ -14,7 +15,7 @@ logreg.fit(X_train, np.ravel(y_train))
 print("Making predictions")
 predictions = logreg.predict(X_dev)
 
-print(metrics.classification_report(y_dev, predictions, target_names=["PERIOD", "QMARK", "EXPOINT"]))
+print(classification_report(y_dev, predictions, target_names=["PERIOD", "QMARK", "EXPOINT"]))
 print("Micro: " + str(metrics.precision_recall_fscore_support(y_dev, predictions, average='micro')))
 print("Macro: " + str(metrics.precision_recall_fscore_support(y_dev, predictions, average='macro')))
 print("Weighted: " + str(metrics.precision_recall_fscore_support(y_dev, predictions, average='weighted')))
